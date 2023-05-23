72079d4d44430f049e2afc1c1a4eb3f62296d843
Jack Wang <jack.wang@integem.com>
2023-05-23 12:30:38 +0800

    update

--- server/connect.py
+++ server/connect.py
@@ -88,9 +88,12 @@ class Resquest(BaseHTTPRequestHandler):
                     print(command)
                     proc = subprocess.Popen(
                         command, shell=True, executable="/bin/bash", preexec_fn=os.setsid)
+                    for line in iter(proc.stdout.readline, b''):
+                        print(line.decode('utf-8'), end='')
+                        self.wfile.write(line)
                     proc.communicate()
-                    buf = "{\"suceesss\": true, \"pid\": %d, \"out\": \"%s\", \"error\": \"%s\"}" % (
-                        proc.pid, proc.stdout.decode('utf-8'), proc.stderr.decode('utf-8'))
+                    buf = "{\"suceesss\": true, \"pid\": %d, \"error\": \"%s\"}" % (
+                        proc.pid, proc.stderr)
                 except Exception as e:
                     buf = "{\"suceesss\": false, \"error\": %s}" % e
             else:
